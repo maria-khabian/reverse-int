1d527b217dc67f14877b737d7f10b56b890182dd
maria-khabian <mariakhabian@gmail.com>
2023-01-09 16:47:03 +0100

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        n = -n;
+    }
+    const nArr = Array.from(n.toString(), Number).reverse().join('')
+    return Number(nArr);
 }
